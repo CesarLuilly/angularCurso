0c57dfe5182f50fb1b092be833d84890013e8274
CesarLuilly <cesarluilly.garcia@hotmail.com>
2021-03-07 11:10:14 -0600

    Video 66 - Metodos en el servicio

--- src/app/dbz/agregar/agregar.component.ts
+++ src/app/dbz/agregar/agregar.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IPersonaje } from '../Interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.services';
 
 @Component({
   selector: 'app-agregar',
@@ -8,42 +9,51 @@ import { IPersonaje } from '../Interfaces/dbz.interface';
 })
 export class AgregarComponent {
 
-    @Input() public nuevo : IPersonaje = {
-      nombre : '',
-      poder : 0
-    }; 
+  @Input() public nuevo : IPersonaje = {
+    nombre : '',
+    poder : 0
+  }; 
 
-    //              //EventEmitter es un tipo Generico
-    //              //   y en este caso voy a emitir personaje.
-    //              //El Output sirve para emitir eventos y entre 
-    //              //  y entre las siguientes llaves  condicionales 
-    //              //  <tipo de dato a enviarse en el evento> se especifica
-    //              //  el tipo de dato que se tiene que enviar.
-    @Output() public onNuevoPersonaje: EventEmitter<IPersonaje> = 
-      new EventEmitter<IPersonaje>();
+  //                //EventEmitter es un tipo Generico
+  //                //   y en este caso voy a emitir personaje.
+  //                //El Output sirve para emitir eventos y entre 
+  //                //  y entre las siguientes llaves  condicionales 
+  //                //  <tipo de dato a enviarse en el evento> se especifica
+  //                //  el tipo de dato que se tiene que enviar.
+  // @Output() public onNuevoPersonaje: EventEmitter<IPersonaje> = 
+  //   new EventEmitter<IPersonaje>();
 
-    public agregar() : void{
-      if(
-        this.nuevo.nombre.trim().length === 0
-      ){
-        return;
-      }
+  //------------------------------------------------------------------------------------------                                
+  constructor(
+    //              //Inyectamos la dependencia del servicio
+    //              //  para que lo puedamos utilizar en cualquier
+    //              //  parte de la clase.
+    private dbzService : DbzService
+  )
+  {  
+    
+  }
 
-      console.log("-- Inicio Componente hijo --")
-      console.log(this.nuevo);
+  //------------------------------------------------------------------------------------------
+  public agregar(
+  //                //Agrega nuevo personaje.
 
-      //            //Aqui se emite el evento.
-      this.onNuevoPersonaje.emit(this.nuevo);
-
-      console.log("-- Fin Componente hijo --")
-
-      this.nuevo = {
-        nombre : "",
-        poder : 0
-      }
-      //              //Con la siguiente linea evitamos 
-      //              //  el evento por defecto(de que se refresque.).
-      // event.preventDefault(); //Pero esto es muy tedioss.
-      
+  ) : void
+  {
+    if(
+      this.nuevo.nombre.trim().length === 0
+    ){
+      return;
     }
+
+    //              //Aqui se emite el evento.
+    // this.onNuevoPersonaje.emit(this.nuevo);
+
+    this.dbzService.agregarPersonaje(this.nuevo);
+
+    this.nuevo = {
+      nombre : "",
+      poder : 0
+    }    
+  }
 }
--- src/app/dbz/services/dbz.services.ts
+++ src/app/dbz/services/dbz.services.ts
@@ -5,42 +5,49 @@ import { IPersonaje } from '../Interfaces/dbz.interface';
 @Injectable()
 export class DbzService {
 
-    private _personajes : IPersonaje[] = [
-        {
-          nombre : "Goku",
-          poder : 10000
-        },
-        {
-          nombre : "Vegeta",
-          poder : 15000
-        }
-      ];
+  private _personajes : IPersonaje[] = [
+      {
+        nombre : "Goku", 
+        poder : 10000
+      },
+      {
+        nombre : "Vegeta",
+        poder : 15000
+      }
+    ];
       
-    //------------------------------------------------------------------------------------------                                
-    constructor(){
-        //          //Hasta que alguien requiera la instancia es cuando
-        //          //  se va a crear la primera instancia, y de ahi en 
-        //          //  adelante se utilzar la misma instantacia para
-        //          //  cualquier componente, servicio, pipe y quien sea
-        //          //  que lo consuma.      
+  //------------------------------------------------------------------------------------------   
+  public get personajes() : IPersonaje[] {
+    //          //El inconveniente, es que javascript todo lo manda
+    //          //  por referencia, y al hacer esto se puede manipualar
+    //          //  la data aunque este marcado como private.
+    // return this._personajes;
 
-        console.log("Servicio Inicializado");
-    }
+    //          //Como buena practica, hay que romper la referencia.
+    //          //Entonces para solucionar este problema, hacemos uso de 
+    //          //  spread syntax(sintaxis extendida) del operador spread,
+    //          //  y lo que hace es separar cada uno de los elementos que 
+    //          //  tiene ese arreglo y se crea uno nuevo, y de esta manera
+    //          //  rompemos la referencia..
+    return [...this._personajes];
+  }   
 
-    //------------------------------------------------------------------------------------------   
-    public get personajes() : IPersonaje[] {
-        //          //El inconveniente, es que javascript todo lo manda
-        //          //  por referencia, y al hacer esto se puede manipualar
-        //          //  la data aunque este marcado como private.
-        // return this._personajes;
+  //------------------------------------------------------------------------------------------                                
+  constructor(){
+      //          //Hasta que alguien requiera la instancia es cuando
+      //          //  se va a crear la primera instancia, y de ahi en 
+      //          //  adelante se utilzar la misma instantacia para
+      //          //  cualquier componente, servicio, pipe y quien sea
+      //          //  que lo consuma.       
+  }
 
-        //          //Como buena practica, hay que romper la referencia.
-        //          //Entonces para solucionar este problema, hacemos uso de 
-        //          //  spread syntax(sintaxis extendida) del operador spread,
-        //          //  y lo que hace es separar cada uno de los elementos que 
-        //          //  tiene ese arreglo y se crea uno nuevo, y de esta manera
-        //          //  rompemos la referencia..
-        return [...this._personajes];
-    }       
+  //------------------------------------------------------------------------------------------
+  public agregarPersonaje( 
+    //              //Agregar personaje.
+
+    personaje : IPersonaje )
+  {
+    this._personajes.push(personaje)
+  }   
 }
 
